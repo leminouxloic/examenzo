85505449ba5ee7b1fd6c4f8c18cd54e52238362a
leminouxloic <loic.lm17@gmail.com>
2020-06-29 11:55:14 +0200

    Command Help refacto

--- src/main/java/com/codurance/training/tasks/Command.java
+++ src/main/java/com/codurance/training/tasks/Command.java
@@ -1,4 +1,14 @@
 package com.codurance.training.tasks;
 
 public class Command {
+
+    public static void help() {
+        out.println("Commands:");
+        out.println("  show");
+        out.println("  add project <project name>");
+        out.println("  add task <project name> <task description>");
+        out.println("  check <task ID>");
+        out.println("  uncheck <task ID>");
+        out.println();
+    }
 }
--- src/main/java/com/codurance/training/tasks/TaskList.java
+++ src/main/java/com/codurance/training/tasks/TaskList.java
@@ -111,15 +111,7 @@ public final class TaskList implements Runnable {
         Projet.setDone(projets, idString, false);
     }
 
-    private void help() {
-        out.println("Commands:");
-        out.println("  show");
-        out.println("  add project <project name>");
-        out.println("  add task <project name> <task description>");
-        out.println("  check <task ID>");
-        out.println("  uncheck <task ID>");
-        out.println();
-    }
+
 
     private void error(String command) {
         out.printf("I don't know what the command \"%s\" is.", command);
