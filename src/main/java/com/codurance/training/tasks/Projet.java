b0232affd91e10d1814253a582eab3afaf3f83c1
leminouxloic <loic.lm17@gmail.com>
2020-06-29 10:54:15 +0200

    paramètres classe projet

--- src/main/java/com/codurance/training/tasks/Projet.java
+++ src/main/java/com/codurance/training/tasks/Projet.java
@@ -1,4 +1,8 @@
 package com.codurance.training.tasks;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Projet {
+    public String name;
+    public List<Task> tasks = new ArrayList<>();
 }
